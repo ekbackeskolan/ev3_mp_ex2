0983d1bfb9020d6dbe044b6f184a0ed039285858
rogersundekbackeskolan <roger.sund@ekbackeskolan.se>
2020-02-15 22:35:17 +0100

    Ver 0.1

--- ev3__mp_ex2/main.py
+++ ev3__mp_ex2/main.py
@@ -10,3 +10,22 @@ from pybricks.robotics import DriveBase
 
 # Write your program here
 brick.sound.beep()
+
+#Exempel hämtat från 
+#https://antonsmindstorms.com/2019/06/15/how-to-run-python-on-an-ev3-brick/
+
+# Clear the display
+brick.display.clear()
+
+# Print ``Hello`` near the middle of the screen
+brick.display.text("Hello", (60, 50))
+
+# Print ``World`` directly underneath it
+brick.display.text("World")
+
+# Show the current voltage
+brick.display.text("Voltage is: {}".format(brick.battery.voltage()))
+
+# Wait until a button is pressed
+while not brick.buttons():
+    wait(10)
